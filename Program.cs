dede78be7d2a25b5233e93906992f12ee7f087ee
Puczk0 <74714430+Puczk0@users.noreply.github.com>
2023-01-24 20:33:49 +0100

    Dodane zadania z numerem 3 i 4.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
 
 internal class Program
 {
@@ -19,17 +21,66 @@ internal class Program
     //         }
     //}
 
+    //{
+    ////Zadanie 2 Wczytać od użytkownika 3 liczby całkowite i wypisać na ekran największą oraz najmniejszą z nich
+    //int[] tablica = new int[3];
+    //Console.WriteLine("Podaj pierwszą liczbę");
+    //tablica[0]= int.Parse(Console.ReadLine());
+    //Console.WriteLine("Podaj drugą liczbę");
+    //tablica[1] = int.Parse(Console.ReadLine());
+    //Console.WriteLine("Podaj trzecią liczbę");
+    //tablica[2] = int.Parse(Console.ReadLine());
+    //Console.WriteLine("Najmniejsza liczba to: " + tablica.Min());
+    //Console.WriteLine("Najmwiększa liczba to: " + tablica.Max());
+
+    //}
+
+
+    //{
+    //    //Zadanie 3 -     Napisać program, który oblicza wartość współczynnika BMI (ang. body mass index) wg.wzoru: wagawzrost2.Jeżeli wynik jest w przedziale(18, 5 - 24, 9) to wypisuje ”waga prawidłowa”, jeżeli poniżej to ”niedowaga”, jeżeli powyżej ”nadwaga”.
+
+    //    Console.WriteLine("Podaj swoją wagę w kilogramach");
+    //    double Weight = double.Parse(Console.ReadLine());
+    //    Console.WriteLine("Podaj swój wzrost w metrach");
+    //    double Height = double.Parse(Console.ReadLine());
+    //    double BMI = Weight / Math.Pow(Height, 2);
+    //    if (Math.Round(BMI) >= 25.0)
+    //    {
+    //        Console.WriteLine("nadwaga");
+    //    }
+    //    else if (Math.Round(BMI) <= 18.4)
+    //    {
+    //        Console.WriteLine("niedowaga");
+    //    }
+    //    else
+    //        Console.WriteLine("prawidłowa waga");
+
+    //}
+    //{
+    //    // Napisać program obliczający należny podatek dochodowy od osób fizycznych.
+    //    // Program ma pobierać od użytkownika dochód i po obliczeniu wypisywać na ekranie należny podatek.
+    //    // Podatek obliczany jest wg. następujących reguł:
+    //    // • do 85.528 podatek wynosi 18 % podstawy minus 556,02 PLN,
+    //    // • od 85.528 podatek wynosi 14.839,02 zł + 32 % nadwyżki ponad 85.528,00
+
+    //    Console.WriteLine("Podaj swój dochód w PLN");
+    //    double income = double.Parse(Console.ReadLine());
+    //    Console.WriteLine("Twój dochód to " + income + " PLN");
+    //    if (income <= 85528.00)
+    //    {
+    //        Console.WriteLine((income * 0.18) - 556.02);
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine((income - 85528.00) * 0.32 + 14839.02);
+
+    //    }
+    //    }
+
     {
-        //Zadanie 2 Wczytać od użytkownika 3 liczby całkowite i wypisać na ekran największą oraz najmniejszą z nich
-        int[] tablica = new int[3];
-        Console.WriteLine("Podaj pierwszą liczbę");
-        tablica[0]= int.Parse(Console.ReadLine());
-        Console.WriteLine("Podaj drugą liczbę");
-        tablica[1] = int.Parse(Console.ReadLine());
-        Console.WriteLine("Podaj trzecią liczbę");
-        tablica[2] = int.Parse(Console.ReadLine());
-        Console.WriteLine("Najmniejsza liczba to: " + tablica.Min());
-        Console.WriteLine("Najmwiększa liczba to: " + tablica.Max());
+    
+    }
+
 
     }
-}
+
